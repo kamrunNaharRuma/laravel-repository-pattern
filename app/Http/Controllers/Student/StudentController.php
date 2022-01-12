910a2c4ac8a26de54e8b9be14c566ca54578bd4e
knahar12bd@gmail.com <knahar12bd@gmail.com>
2022-01-12 17:33:00 +0600

    interface injected

--- app/Http/Controllers/Student/StudentController.php
+++ app/Http/Controllers/Student/StudentController.php
@@ -3,9 +3,21 @@
 namespace App\Http\Controllers\Student;
 
 use App\Http\Controllers\Controller;
+use App\Models\Student;
+use App\Repository\Student\StudentInterface;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
+use function is_null;
+use function redirect;
 
 class StudentController extends Controller
 {
     //
+    protected $student;
+    public function __construct(StudentInterface $student){
+        $this->student = $student;
+    }
+    public function index(){
+        return $this->student->getAllData();
+    }
 }
