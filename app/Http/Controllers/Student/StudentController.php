1d924379110813e483f78c459a255b992faebea6
knahar12bd@gmail.com <knahar12bd@gmail.com>
2022-01-12 18:09:38 +0600

    corrected path

--- app/Http/Controllers/Student/StudentController.php
+++ app/Http/Controllers/Student/StudentController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Student;
 
 use App\Http\Controllers\Controller;
 use App\Models\Student;
-use App\Repository\Student\StudentInterface;
+use App\Repository\Student\StudentRepository;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\View;
 use function is_null;
@@ -12,13 +12,40 @@ use function redirect;
 
 class StudentController extends Controller
 {
-    //
     protected $student;
-    public function __construct(StudentInterface $student){
+    public function __construct(StudentInterface $student)
+    {
         $this->student = $student;
     }
+
     public function index(){
-        dd("here");
-        return $this->student->getAllData();
+        if(View::exists('student.index')){
+            return \view('student.index',[
+                'students' => $this->student->getAllData(),
+                'trashed' => $this->student->getAllTrashedData()
+            ]);
+        }
+    }
+
+    public function storeOrUpdate(Request $request,$id = null){
+        $data = $request->only(['name','roll']);
+        if(!is_null($id)){ //update
+            $this->student->storeOrUpdate($id,$data);
+            return redirect()->route('student.index')->with('message','Data Updated!');
+        }else{//insert
+            $this->student->storeOrUpdate($id = null,$data);
+            return redirect()->route('student.index')->with('message','Data Inserted!');
+        }
+    }
+
+    public function view($id){
+        if(View::exists('student.edit')){
+            return view('student.edit',['student' => $this->student->view($id)]);
+        }
+    }
+
+    public function delete($id){
+        $this->student->delete($id);
+        return redirect()->route('student.index')->with('message','Data Deleted!');
     }
 }
