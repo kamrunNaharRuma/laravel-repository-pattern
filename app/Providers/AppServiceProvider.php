2c359ba678880374686feb1c480d7cc9a8500ba0
knahar12bd@gmail.com <knahar12bd@gmail.com>
2022-01-12 17:26:35 +0600

    binding interface an repository class

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Repository\Student\StudentInterface;
+use App\Repository\Student\StudentRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,6 +16,7 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+        $this->app->bind(StudentInterface::class, StudentRepository::class);
     }
 
     /**
